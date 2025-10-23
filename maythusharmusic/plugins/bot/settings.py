728d97fad6b5b14cf25c26ed5bfc72397bf74ddd
nan20psp <pyaesone10psp@gmail.com>
2025-10-23 17:34:50 +0630

    Update settings.py

--- maythusharmusic/plugins/bot/settings.py
+++ maythusharmusic/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://files.catbox.moe/jy8o13.mp4"),
+        InputMediaVideo("https://files.catbox.moe/0tuzil.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
@@ -405,3 +405,4 @@ async def vote_change(client, CallbackQuery, _):
         )
     except MessageNotModified:
         return
+
