bc0d7dfff791daa6464a480e6875715cc5076f27
nan21psp <pyaesone35psp@gmail.com>
2025-11-19 21:33:35 +0630

    Update __init__.py

--- maythusharmusic/utils/inline/__init__.py
+++ maythusharmusic/utils/inline/__init__.py
@@ -1,33 +1,9 @@
-from .start import (
-    start_panel,
-    private_panel,
-    clone_private_panel, # <--- ဒီဟာ ပါမပါ စစ်ဆေးပြီး ထည့်ပေးပါ
-)
-from .help import (
-    first_page,
-    second_page,
-    help_pannel,
-    help_back_markup,
-    private_help_panel,
-)
-from .play import (
-    botplaylist_markup,
-    top_play_markup,
-    get_playlist_markup,
-    failed_top_markup,
-    warning_markup,
-    close_markup,
-)
-from .song import song_markup
-from .settings import (
-    setting_markup,
-    vote_mode_markup,
-    auth_users_markup,
-    playmode_users_markup,
-    audio_quality_markup,
-    video_quality_markup,
-)
-from .stats import stats_buttons, back_stats_buttons
-from .speed import speed_markup
-from .extras import botplaylist_markup, close_markup, supp_markup
-from .queue import aq_markup
+from .extras import *
+from .help import *
+from .play import *
+from .queue import *
+from .settings import *
+from .speed import *
+from .start import *
+from .sudolist import *
+from .playlist import *
