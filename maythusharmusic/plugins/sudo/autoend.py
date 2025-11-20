7fb52fefeffa44ac5a89d79fb713c285e9909f54
nan21psp <pyaesone35psp@gmail.com>
2025-11-21 00:38:07 +0630

    Update autoend.py

--- maythusharmusic/plugins/sudo/autoend.py
+++ maythusharmusic/plugins/sudo/autoend.py
@@ -5,16 +5,12 @@ from maythusharmusic import app
 from maythusharmusic.misc import SUDOERS
 from maythusharmusic.utils.database import autoend_off, autoend_on
 
-# Autoend ကို default အနေနဲ့ enable လုပ်ထားမယ်
-# ဒီ value ကိုပြင်ရုံနဲ့ autoend status ပြောင်းသွားမယ်
-AUTOEND_ENABLED = True
 
 @app.on_message(filters.command("autoend") & SUDOERS)
 async def auto_end_stream(_, message: Message):
     usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         await autoend_on()
@@ -26,18 +22,3 @@ async def auto_end_stream(_, message: Message):
         await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
     else:
         await message.reply_text(usage)
-
-# Autoend status ကိုစစ်ဆေးပြီးသတ်မှတ်မယ်
-async def set_autoend_status():
-    try:
-        if AUTOEND_ENABLED:
-            await autoend_on()
-            print("🎵 Autoend feature is ENABLED by default")
-        else:
-            await autoend_off() 
-            print("🎵 Autoend feature is DISABLED by default")
-    except Exception as e:
-        print(f"Error setting autoend status: {e}")
-
-# Function ကိုခေါ်သုံးမယ်
-set_autoend_status()
