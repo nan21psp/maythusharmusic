2415025d5e6fd6906229ca946994b8a31f367bf0
nan21psp <pyaesone35psp@gmail.com>
2025-11-20 15:00:18 +0630

    Update start.py

--- maythusharmusic/utils/inline/start.py
+++ maythusharmusic/utils/inline/start.py
@@ -18,22 +18,15 @@ def start_panel(_):
 
 def private_panel(_):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
-        ],
-    ]
-    return buttons
+    [InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/{app_username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users)],
+    [
+        InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+    ],
+    [
+        InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+    ],
+    [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+]
+return buttons
