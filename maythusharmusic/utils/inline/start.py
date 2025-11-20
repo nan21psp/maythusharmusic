b501e4615fc9e3fd3633839a2c9edd0b5e529fea
nan21psp <pyaesone35psp@gmail.com>
2025-11-20 08:48:24 +0630

    Update start.py

--- maythusharmusic/utils/inline/start.py
+++ maythusharmusic/utils/inline/start.py
@@ -37,21 +37,3 @@ def private_panel(_):
         ],
     ]
     return buttons
-
-def clone_private_panel(_, bot_username):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["Aᴅᴅ ᴍᴇ ʙᴀʙʏ"],
-                url=f"https://t.me/{app_username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+ban_users"
-            )
-        ],
-        [
-            InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/iwillgoforwardsalone"),
-            InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="https://t.me/sasukemusicsupportchat"),
-        ],
-        [
-            InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ", url="https://t.me/everythingreset"),
-        ],
-    ]
-    return buttons
