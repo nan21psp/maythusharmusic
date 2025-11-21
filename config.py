c794aae52af44a1f0fb2777c1064e7373a49c46d
nan21psp <pyaesone35psp@gmail.com>
2025-11-21 23:51:03 +0630

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL_LINK = getenv("SUPPORT_CHANNEL_LINK", "@everythingreset")
-SUPPORT_CHAT_LINK = getenv("SUPPORT_CHAT_LINK", "@sasukemusicsupportchat")
+SUPPORT_CHANNEL_LINK = getenv("SUPPORT_CHANNEL_LINK", "https://t.me/everythingreset")
+SUPPORT_CHAT_LINK = getenv("SUPPORT_CHAT_LINK", "https://t.me/sasukemusicsupportchat")
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sasukevipmusicbotsupport")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sasukemusicsupportchat")
