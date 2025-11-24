fbee29eb9a9754de58768ca2ba174e31253588fc
nan21psp <pyaesone35psp@gmail.com>
2025-11-24 23:07:49 +0630

    Update play.py

--- maythusharmusic/plugins/play/play.py
+++ maythusharmusic/plugins/play/play.py
@@ -7,7 +7,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from maythusharmusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from maythusharmusic.core.call import pisces
+from maythusharmusic.core.call import Hotty
 from maythusharmusic.utils import seconds_to_min, time_to_seconds
 from maythusharmusic.utils.channelplay import get_channeplayCB
 from maythusharmusic.utils.decorators.language import languageCB
@@ -289,7 +289,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await pisces.stream_call(url)
+                await Hotty.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
@@ -531,7 +531,7 @@ async def piyush_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("piscesPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("HottyPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
