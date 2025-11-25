3c97edb622ac93c3a06a14b78b9dec83df224533
nan21psp <pyaesone35psp@gmail.com>
2025-11-25 18:21:45 +0630

    Update autoleave.py

--- maythusharmusic/plugins/misc/autoleave.py
+++ maythusharmusic/plugins/misc/autoleave.py
@@ -1,74 +1,54 @@
 import asyncio
 from datetime import datetime
+
 from pyrogram.enums import ChatType
-from pyrogram.types import Message
-from pyrogram import Client, filters
+
 import config
 from maythusharmusic import app
 from maythusharmusic.core.call import Hotty, autoend
-from maythusharmusic.misc import SUDOERS
-from maythusharmusic.utils.database import (
-    get_client,
-    is_active_chat,
-    is_autoend,
-    get_autoleave,
-    set_autoleave,
-)
+from maythusharmusic.utils.database import get_client, is_active_chat, is_autoend
 
-# --------------------- AUTO LEAVE SYSTEM ---------------------
 
 async def auto_leave():
-    while True:
-        await asyncio.sleep(25200)  # 7 hours
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(25200):
+            from maythusharmusic.core.userbot import assistants
 
-        # database ထဲက autoleave status check
-        status = await get_autoleave()
-        if not status:
-            continue
-
-        from maythusharmusic.core.userbot import assistants
-
-        for num in assistants:
-            client = await get_client(num)
-            left = 0
-
-            try:
-                async for i in client.get_dialogs():
-                    if i.chat.type in [
-                        ChatType.SUPERGROUP,
-                        ChatType.GROUP,
-                        ChatType.CHANNEL,
-                    ]:
-                        if i.chat.id not in [
-                            config.LOGGER_ID,
-                            -1002459775779,
-                            -1002356385851,
+            for num in assistants:
+                client = await get_client(num)
+                left = 0
+                try:
+                    async for i in client.get_dialogs():
+                        if i.chat.type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
                         ]:
-                            if left == 150:
-                                continue
-
-                            if not await is_active_chat(i.chat.id):
-                                try:
-                                    await client.leave_chat(i.chat.id)
-                                    left += 1
-                                except:
+                            if (
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1002459775779
+                                and i.chat.id != -1002356385851
+                            ):
+                                if left == 150:
                                     continue
-            except:
-                pass
+                                if not await is_active_chat(i.chat.id):
+                                    try:
+                                        await client.leave_chat(i.chat.id)
+                                        left += 1
+                                    except:
+                                        continue
+                except:
+                    pass
 
 
 asyncio.create_task(auto_leave())
 
-# --------------------- AUTO END STREAM ---------------------
 
 async def auto_end():
-    while True:
-        await asyncio.sleep(5)
-
+    while not await asyncio.sleep(5):
         ender = await is_autoend()
         if not ender:
             continue
-
         for chat_id in autoend:
             timer = autoend.get(chat_id)
             if not timer:
@@ -92,25 +72,3 @@ async def auto_end():
 
 
 asyncio.create_task(auto_end())
-
-# --------------------- TELEGRAM COMMAND ---------------------
-
-@app.on_message(filters.command(["autoleave"]) & SUDOERS)
-async def autoleave_cmd(client: Client, message: Message):
-    if len(message.command) < 2:
-        return await message.reply(
-            "**Usage:** `/autoleave enable` or `/autoleave disable`"
-        )
-
-    query = message.command[1].lower()
-
-    if query == "enable":
-        await set_autoleave(True)
-        return await message.reply("✅ **Auto Leave Enabled.**")
-
-    elif query == "disable":
-        await set_autoleave(False)
-        return await message.reply("❌ **Auto Leave Disabled.**")
-
-    else:
-        return await message.reply("Invalid option. Use: enable / disable")
